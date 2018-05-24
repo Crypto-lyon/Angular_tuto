d76bbf3d904816cb7d69bf87b64540a7e5274329
Phylante <quentin.vaudaine@gmail.com>
2018-05-24 20:16:21 +0200

    On change la politique de detection des changements de notre component todo-view

--- src/app/todo/todo-view/todo-view.component.ts
+++ src/app/todo/todo-view/todo-view.component.ts
@@ -1,13 +1,16 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input, OnInit} from '@angular/core';
 import {Todo} from "../todo";
 
 @Component({
+  // changeDetectionStrategy : Le component se mettra à jour, uniquement lorsqu'un objet immutable passera dans l'input.
+  changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-todo-view',
   templateUrl: './todo-view.component.html',
   styleUrls: ['./todo-view.component.css']
 })
 export class TodoViewComponent implements OnInit {
 
+  // On crée un input pour pouvoir boucler dans le template de todo-list.component.ts
   @Input() item: Todo;
   constructor() { }
 
